362d19649f35da00a7416f009dc4b0c1fae1b717
GiorgettaR <giorgetta.roberto@gmail.com>
2024-08-22 18:16:23 +0200

    FIX: app name and project route name

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/projects', [ProjectController::class, 'index']);
-Route::get('/projects/{project:slug}', [ProjectController::class, 'show']);
+Route::get('/project/{project:slug}', [ProjectController::class, 'show']);
 Route::post('/contacts', [LeadController::class, 'store']);
